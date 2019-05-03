7ef076c26feda10d5f8deb9ac3befe47f1afc1b7
Efe Özgür <efecuneytozgur@gmail.com>
2019-05-03 07:32:14 +0300

    Update Root.js

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
 import {Router, Scene} from 'react-native-router-flux';
 import Main from './components/Main';
 import Sonuc from './components/Sonuc';
-
-
 
 class Root extends Component {
     render(){
@@ -12,13 +9,12 @@ class Root extends Component {
             <Router>
                 <Scene key = 'Root'> 
                     <Scene key = 'Main'  title="Anasayfa" initial component={Main} />
-                    <Scene key = 'sonuc' rightTitle="Yeni" onRight={()=>console.log("deli")} titleStyle={{color:'black'}} title="Sonuç Gösteriliyor" tabs component={Sonuc} />
+                    <Scene key = 'sonuc' rightTitle="Yeni" onRight={()=>console.log("xxx")} titleStyle={{color:'black'}} title="Sonuç Gösteriliyor" component={Sonuc} />
                 </Scene>
             </Router>
         ); 
     }
 }
-
 
 export default Root; 
 
